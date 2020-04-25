92cafb8b539928b4225582cfe4cb9eb8f4bf32d4
mattkohl82 <mattkohl82@gmail.com>
2020-04-25 12:09:48 -0500

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ var windEl = document.querySelector(".wind");
 var clock = document.querySelector("#currentDay");
 const apiKey = '00f12fe173cc605c324a6464e403da46';
 
+const citySearch = document.querySelector("#citySearch");
+
+var currentBox = document.querySelector("#current-box");
 
 function updateTime() {
     const now = moment();
@@ -21,9 +24,9 @@ setInterval(updateTime, 100);
 updateTime();
 
 function myFunction() {
-    const citySearch = document.querySelector("#citySearch").value;
     
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&APPID=' + apiKey + '&units=imperial')
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySearch.value + '&APPID=' + apiKey + '&units=imperial')
 
     .then((response) => {
       return response.json();
@@ -34,26 +37,66 @@ function myFunction() {
         let cityValue = data['name'];
         //let icon = data['weather']['icon'];
         let temp = data['main']['temp'];
-        let highTemp = data['main']["temp_max"];
-        let lowTemp = data['main']["temp_min"];
+        let highTemp = data['main']['temp_max'];
+        let lowTemp = data['main']['temp_min'];
         let humidity = data['main']['humidity'];
         let windSpeed = data['wind']['speed'];
+        
 
-            cityEl.innerHTML = cityValue;
-            tempEl.innerHTML = 'Temperature: ' + temp + 'F';
-            highEl.innerHTML = 'High: ' + highTemp + 'F';
-            lowEl.innerHTML = 'Low: ' + lowTemp +'F';
-            humidityEl.innerHTML = 'Humidity: ' + humidity + '%';
-            windEl.innerHTML = 'Wind: ' + windSpeed + ' MPH';
+          cityEl.innerHTML = cityValue;
+          tempEl.innerHTML = 'Temperature: ' + temp + 'F';
+          highEl.innerHTML = 'High: ' + highTemp + 'F';
+          lowEl.innerHTML = 'Low: ' + lowTemp +'F';
+          humidityEl.innerHTML = 'Humidity: ' + humidity + '%';
+          windEl.innerHTML = 'Wind: ' + windSpeed + ' MPH';
+          currentBox.style.borderStyle = 'dashed';
 
-  
+  });
+}
 
 
-            
-            console.log(cityValue, temp, humidity, windSpeed,highTemp , lowTemp);
 
-    });
+function fiveDay() {
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch.value + '&APPID=' + apiKey + '&units=imperial')
+
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+
+    for (var i = 0; i < 33; i += 8) {
+    let temp = data['list'][i]['main']['temp'];
+      console.log(data);
+    
+    
+    var tempEl = document.querySelector(`#temp${i}`);
+
+    tempEl.textContent = 'Temp: ' + temp;
   }
+})
+}
+
+
+
+document.getElementById("search").addEventListener("click", function() {
+  fiveDay();
+  myFunction();
+
+  let searchHistory = localStorage.getItem("searchHistory");
+  if(searchHistory) {
+    searchHistory = JSON.parse(searchHistory)
+    searchHistory.push(citySearch.value);
+
+  } else {
+    searchHistory = [citySearch.value];
+     
+  }
+
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+});
+
+
+
 
   
 
